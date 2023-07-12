2f445bc0983afde3563ea85b48932a44289af64a
LPRegen <manuel.escribano.051@gmail.com>
2023-07-12 21:05:29 +0200

    refactor: remove config from package.json to its own file

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Options} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+  singleQuote: true,
+  semi: true,
+};
+
+module.exports = config;
