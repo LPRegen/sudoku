938b7197c441a7401b7851717e1ba11b3181f969
LPRegen <manuel.escribano.051@gmail.com>
2023-07-14 07:43:30 +0200

    style: modified code style with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,17 +9,17 @@ module.exports = {
     "plugin:jest-dom/recommended",
     "plugin:jsx-a11y/strict",
     "plugin:testing-library/react",
-    "prettier"
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn",
     "no-console": 2,
-    "@typescript-eslint/no-explicit-any": 2
+    "@typescript-eslint/no-explicit-any": 2,
   },
 };
--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   "src/**/*.{ts,tsx}": "eslint --cache --fix",
-  "src/**/*.{ts,tsx,css,scss,md}": "prettier --write --ignore-unknown"
+  "src/**/*.{ts,tsx,css,scss,md}": "prettier --write --ignore-unknown",
 };
