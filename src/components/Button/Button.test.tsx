c577859ca14e8e5511cac69ea3dca02e084eb2a8
LPRegen <manuel.escribano.051@gmail.com>
2023-07-27 17:28:33 +0200

    refactor: add handleClick

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,18 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import { Button } from './Button';
 
 describe('Button test', () => {
   it('Renders the component', () => {
-    render(<Button>Test</Button>);
+    render(<Button handleClick={jest.fn()}>Test</Button>);
     const buttonEl = screen.getByRole('button');
     expect(buttonEl).toBeInTheDocument();
   });
 
   it('Button is correctly aria-labeled', () => {
-    render(<Button>New game</Button>);
+    render(<Button handleClick={jest.fn()}>New game</Button>);
     const buttonEl = screen.getByRole('button', { name: 'New game' });
     expect(buttonEl).toBeInTheDocument();
+  });
+
+  it('handleClick function is executed correctly on click', async () => {
+    render(<Button handleClick={jest.fn()}>New game</Button>);
+    const buttonEl = screen.getByRole('button');
+    await userEvent.click(buttonEl);
   });
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   children: string;
+  handleClick: () => void;
 }
 
-export const Button = ({ children }: ButtonProps) => {
-  return <button>{children}</button>;
+export const Button = ({ children, handleClick }: ButtonProps) => {
+  return <button onClick={handleClick}>{children}</button>;
 };
