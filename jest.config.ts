52caab496d437623b415d129622543fdc40a6a58
LPRegen <manuel.escribano.051@gmail.com>
2023-07-27 22:44:14 +0200

    refactor: configure testing env - Add setup file for jest - Specify root directory and setupFilesAfterEnv for jest - Include jest setup file in tsconfig

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  rootDir: './',
   verbose: true,
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 export default config;
