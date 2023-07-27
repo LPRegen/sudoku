e6df074d70b503ec1ca7a4725634719f13f701bc
LPRegen <manuel.escribano.051@gmail.com>
2023-07-27 23:07:52 +0200

    refactor: remove unnecessary props

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,25 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
 
 import { Button } from './Button';
 
 describe('Button test', () => {
   it('Renders the component', () => {
-    render(<Button handleClick={jest.fn()}>Test</Button>);
+    render(<Button>Test</Button>);
     const buttonEl = screen.getByRole('button');
     expect(buttonEl).toBeInTheDocument();
   });
 
   it('Button is correctly aria-labeled', () => {
-    render(<Button handleClick={jest.fn()}>New game</Button>);
+    render(<Button>New game</Button>);
     const buttonEl = screen.getByRole('button', { name: 'New game' });
     expect(buttonEl).toBeInTheDocument();
   });
 
   it('handleClick function is executed correctly on click', async () => {
     const handleClick = jest.fn();
-    render(<Button handleClick={handleClick}>New game</Button>);
+    render(<Button onClick={handleClick}>New game</Button>);
     const buttonEl = screen.getByRole('button');
     await userEvent.click(buttonEl);
     expect(handleClick).toHaveBeenCalled();
