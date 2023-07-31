9df4126b9e9cabdfeb36bc69d2aa009da5c1a2a3
LPRegen <manuel.escribano.051@gmail.com>
2023-08-01 01:05:38 +0200

    style: code style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,27 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:jest-dom/recommended', 'plugin:jsx-a11y/strict', 'plugin:testing-library/react', 'prettier', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:jsx-a11y/strict',
+    'plugin:testing-library/react',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'no-console': 2,
-    '@typescript-eslint/no-explicit-any': 2
-  }
+    '@typescript-eslint/no-explicit-any': 2,
+  },
 };
