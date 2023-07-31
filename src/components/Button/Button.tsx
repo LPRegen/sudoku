c9ea9b8999d424aee8ddf8608518271d9b349c8f
LPRegen <manuel.escribano.051@gmail.com>
2023-08-01 00:10:36 +0200

    refactor: add styles to component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Button.styles.scss';
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   children: React.ReactNode;
 }
 
 export const Button = ({ children, ...rest }: ButtonProps) => {
-  return <button {...rest}>{children}</button>;
+  return (
+    <button {...rest} className="btn">
+      {children}
+    </button>
+  );
 };
