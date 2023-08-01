37db41f0471eff53e162a7b2480bb69ef0bd11d8
LPRegen <manuel.escribano.051@gmail.com>
2023-08-01 05:41:57 +0200

    refactor: add Button stories - add stories for different difficulty modes

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -23,3 +23,15 @@ export const NewGame: Story = {
 export const ResumeGame: Story = {
   render: () => <Button>Resume game</Button>,
 };
+
+export const EasyDifficulty: Story = {
+  render: () => <Button>Easy</Button>,
+};
+
+export const MediumDifficulty: Story = {
+  render: () => <Button>Medium</Button>,
+};
+
+export const HardDifficulty: Story = {
+  render: () => <Button>Hard</Button>,
+};
